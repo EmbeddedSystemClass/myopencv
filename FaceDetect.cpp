a1611f95054d02058b7946e5ce34bfef669ce2b4
Noom <asupawat@users.noreply.github.com>
2019-06-01 21:28:41 +0700

    Create FaceDetect.cpp

---
+++ FaceDetect.cpp
@@ -0,0 +1,49 @@
+/** @mainpage qwavecv - None
+ *
+ * @author Supawat A <asupawat@gmail.com>
+ * @version 1.0.0
+**/
+
+
+#include "qwavecv.h"
+#include "opencv2/imgproc/imgproc.hpp"
+#include "opencv2/objdetect/objdetect.hpp"
+
+
+EXTERN_C void QwaveCV_FaceDetect(lvError * errorHandle, CVImageHandle *hImage) {
+    CVERROR error = ERR_SUCCESS;
+    QwaveCV_ReturnOnPreviousError(errorHandle);
+    try
+    {
+        if (!hImage) {
+            QwaveCV_ThrowCVError(ERR_NULL_POINTER);
+        }
+
+        Mat image = QwaveCV_LVToMat(hImage);
+	// Add your code here!
+	
+	// Load Face cascade (.xml file)
+    CascadeClassifier face_cascade;
+    face_cascade.load( "/c/vision/haarcascade_frontalface_alt2.xml" );
+ 
+    // Detect faces
+    std::vector<Rect> faces;
+    face_cascade.detectMultiScale( image, faces, 1.1, 2, 0|CV_HAAR_SCALE_IMAGE, Size(30, 30) );
+ 
+    // Draw circles on the detected faces
+    for( int i = 0; i < faces.size(); i++ )
+    {
+        Point center( faces[i].x + faces[i].width*0.5, faces[i].y + faces[i].height*0.5 );
+        ellipse( image, center, Size( faces[i].width*0.5, faces[i].height*0.5), 0, 0, 360, Scalar( 255, 0, 255 ), 4, 8, 0 );
+    }
+
+        QwaveCV_MatToLV(hImage, image);
+    }
+    catch(CVERROR _err){
+        error = _err;
+    }
+    catch (...){
+        error = ERR_OCV_USER;
+    }
+    QwaveCV_ProcessCVError(error, errorHandle);
+}
